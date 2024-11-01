67ea41eceeff836bb8bccde874ccb92af7b0896a
Daniel <danieltr3000@gmail.com>
2024-11-01 16:53:47 -0300

    changes

---
+++ src/main/java/name/zenn/tutorial/TutorialMod.java
@@ -0,0 +1,17 @@
+package name.zenn.tutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod"; // the mod ID can't have uppercase or spaces
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/name/zenn/tutorial/TutorialModClient.java
@@ -0,0 +1,10 @@
+package name.zenn.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
